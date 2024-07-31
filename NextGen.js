25a59035cdc15ee2d283f7bacf8d877b89ada754
Supersonic-Seven <benjaminrockholtz@TandemEDUGA.onmicrosoft.com>
2024-07-31 20:37:40 +0000

    edited base, added graphics

--- NextGen.js
+++ NextGen.js
@@ -1,3 +1,7 @@
 //This is a test. I hope this will work eventually.
-var test = true;
-print(test);
+var test = 0;
+draw = function() {
+    background(0, 0, 0);
+    test += 1;
+    print(test);
+}
